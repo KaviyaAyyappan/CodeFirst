ce20ddf7edbfe6a9f9f29fbb239bb6790256a61d
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-08-07 16:21:40 +0200

    first commit

---
+++ LexiconUniversity/LexiconUniversity/Models/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LexiconUniversity.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
